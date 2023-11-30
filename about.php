512964bb90c9ae6e5012f1183a192e4f1338263d
pradiptechie <kumarpradip3956@gmail.com>
2023-12-01 05:29:13 +0545

    b

--- about.php
+++ about.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/about.css">
     <title>Farm Management System</title>
 </head>
 <body>
--- contact.php
+++ contact.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/contact.css">
+
     <title>Farm Management System</title>
     <!-- <script >
         function alphaOnly(event) {
